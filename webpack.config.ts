4b3bb53c010a7e3c6dade0212c5e7980330dad6a
Andrey Kultin <and.kultin@yandex.ru>
2022-09-14 19:06:04 +0300

    error  ts.resolveTypeReferenceDirective solved

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,8 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+import path from 'path'
+import webpack from 'webpack'
+import HTMLWebpackPlugin from 'html-webpack-plugin'
 
-module.exports = {
+const config: webpack.Configuration = {
   mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
@@ -30,3 +30,5 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
 }
+
+export default config;
