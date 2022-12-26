e5024dbe4f2a38a74b64530b4befce8ed23a12d5
Andrey Kultin <and.kultin@yandex.ru>
2022-12-26 17:54:02 +0800

    interceptor

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,12 @@ import { USER_LOCAL_STORAGE_KEY } from 'shared/consts/localStorage';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '',
-    },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
+    }
+
+    return config;
 });
