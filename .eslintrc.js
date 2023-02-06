ff2796830d64f338df9399035703d4369d684a29
Andrey Kultin <and.kultin@yandex.ru>
2023-02-06 16:09:51 +0800

    linter updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -67,7 +67,7 @@ module.exports = {
         'no-param-reassign': 'off',
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
-        'paths-react-aditional/path-checker': 'error',
+        'paths-react-aditional/path-checker': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,
