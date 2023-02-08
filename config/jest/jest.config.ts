58d17a5f03228c84bd837dc3b890b8865229123a
Andrey Kultin <and.kultin@yandex.ru>
2023-02-08 21:02:21 +0800

    screenshots approved

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -38,6 +38,7 @@ export default {
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+        '^@/(.*)$': '<rootDir>/src/$1',
     },
     reporters: [
         'default',
--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -14,6 +14,7 @@ export const parameters = {
             date: /Date$/,
         },
     },
+    layout: 'fullscreen',
 };
 
 addDecorator(StyleDecorator);
