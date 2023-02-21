8b5c59d4f6db0bd97dbff7ca7ed68f98b3e166e0
Andrey Kultin <and.kultin@yandex.ru>
2023-02-21 17:37:01 +0800

    changed dist to build

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,18 +1,8 @@
 import { ResolveOptions } from 'webpack';
-import path from 'path';
 import { BuildOptions } from './types/config';
 
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
-
 export function buildResolvers(options: BuildOptions): ResolveOptions {
-  
-  console.log('#####################');
-  console.log('__dirname', path.resolve(__dirname));
-  console.log('__dirname, src', path.resolve(__dirname, 'src'));
-  console.log('options.paths.src', options.paths.src);
-  console.log('#####################');
-  
+
     return {
         extensions: ['.tsx', '.ts', '.js'],
         preferAbsolute: true,
--- webpack.config.ts
+++ webpack.config.ts
@@ -6,11 +6,11 @@ import { BuildEnv, BuildPaths } from './config/build/types/config';
 export default (env: BuildEnv) => {
     const paths: BuildPaths = {
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
-        build: path.resolve(__dirname, 'dist'),
+        build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
         src: path.resolve(__dirname, 'src'),
         locales: path.resolve(__dirname, 'public', 'locales'),
-        buildLocales: path.resolve(__dirname, 'dist', 'locales'),
+        buildLocales: path.resolve(__dirname, 'build', 'locales'),
     };
     
 
