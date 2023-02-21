5bd38e80c656a6dc7407573c61461d0da004c96e
Andrey Kultin <and.kultin@yandex.ru>
2023-02-21 13:47:02 +0800

    changed src path

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -11,7 +11,6 @@ interface LoginModalProps {
 }
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
-  
     <Modal
         isOpen={isOpen}
         onClose={onClose}
