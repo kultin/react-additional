086247e2efad4d11a6c67148bc0fc4d2ccf440dd
Andrey Kultin <and.kultin@yandex.ru>
2023-03-07 23:04:58 +0800

    added path react-additional

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -23,7 +23,7 @@ export function buildResolvers(options: BuildOptions): ResolveOptions {
         modules: [options.paths.src, 'node_modules'],
         mainFiles: ['index'],
         alias: {
-            '@': options.paths.src,
+            '@': `/react-additional/${options.paths.src}`,
         },
     };
 }
